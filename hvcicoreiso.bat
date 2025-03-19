b2375fd69fe1602ed306afe3521336ee3c23a6ab
larcencial <larcencial.06@gmail.com>
2025-03-19 11:15:46 +0300

    Update hvcicoreiso.bat

--- hvcicoreiso.bat
+++ hvcicoreiso.bat
@@ -6,50 +6,51 @@ color 4
 echo.
 powershell -Command "$fileContent = Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/larcencial/freedom/refs/heads/main/fafu.txt'; Write-Host $fileContent"
 echo.
+echo The script is working, please do not interrupt.
 timeout /t 4 /nobreak >nul
 
 echo =======================================================================
 powershell -Command "$result = Set-ProcessMitigation -System -Disable ForceRelocateImages; if ($?) { Write-Host 'Virtualization Status: Successfully Disabled' } else { Write-Host 'Virtualization Status: Failed to disable' }"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 echo =======================================================================
 powershell -Command "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -NoRestart; if ($?) { Write-Host 'Hyper-V Successfully Disabled' } else { Write-Host 'Failed to disable Hyper-V' }"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 echo =======================================================================
 powershell -Command "cmd /c 'bcdedit /set {current} hypervisorlaunchtype off && echo HypervisorLaunchType Successfully Disabled || echo Failed to disable HypervisorLaunchType'"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 echo =======================================================================
 powershell -Command "cmd /c 'bcdedit /set hypervisorlaunchtype off && echo HypervisorLaunchType(M2) Successfully Disabled || echo Failed to disable HypervisorLaunchType(M2)'"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 echo =======================================================================
 powershell -Command "Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\DeviceGuard' -Name 'EnableVirtualizationBasedSecurity' -Value 0; Write-Host 'VBS(Virtualization Based Security) Disabled Successfully'"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 echo =======================================================================
 powershell.exe -Command "Set-ItemProperty -Path 'HKLM:\\System\\CurrentControlSet\\Control\\Lsa' -Name 'LsaCfgFlags' -Value 0; if ($?) { Write-Host 'LsaCfgFlags Successfully Set to 0' } else { Write-Host 'Failed to set LsaCfgFlags' }"
-timeout /t 2 /nobreak >nul
 echo =======================================================================
+timeout /t 2 /nobreak >nul
 echo.
 echo.
 
 @echo off
 echo =======================================================================
 echo To apply the changes, you need to restart your computer.
-timeout /t 3 /nobreak >nul
 echo =======================================================================
+timeout /t 3 /nobreak >nul
 echo.
 echo.
 echo 1 - EXIT
